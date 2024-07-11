2d9a8c102adde47d26fb05121a57f724f6658ed6
c0a21086bb <85671034+c0a21086bb@users.noreply.github.com>
2024-07-11 15:30:40 +0900

    Update receive.py

--- receive.py
+++ receive.py
@@ -2,32 +2,32 @@ import network
 import espnow
 import time
 
-while True:
-    try:
-        # A WLAN interface must be active to send()/recv()
-        sta = network.WLAN(network.STA_IF)
-        sta.active(True)
-        sta.disconnect()   # Because ESP8266 auto-connects to last Access Point
 
-        e = espnow.ESPNow()
-        e.active(True)
+try:
+    # A WLAN interface must be active to send()/recv()
+    sta = network.WLAN(network.STA_IF)
+    sta.active(True)
+    sta.disconnect()   # Because ESP8266 auto-connects to last Access Point
 
-        while True:
-            host, msg = e.recv()
-            if msg:             # msg == None if timeout in recv()
-                print(host, msg)
-                if msg == b'end':
-                    break
-            
-        e.active(False)
-        sta.active(False)
-    
-        time.sleep(60)
+    e = espnow.ESPNow()
+    e.active(True)
 
-    except Exception as e:
-        f = open("error.log", "a")
-        f.write(str(e))
-        f.write("\n")
-        f.close()
-	e.active(False)
-        sta.active(False)
+    while True:
+        host, msg = e.recv()
+        if msg:             # msg == None if timeout in recv()
+            print(host, msg)
+            if msg == b'end':
+                break
+        
+    e.active(False)
+    sta.active(False)
+
+    time.sleep(60)
+
+except Exception as e:
+    f = open("error.log", "a")
+    f.write(str(e))
+    f.write("\n")
+    f.close()
+    e.active(False)
+    sta.active(False)
